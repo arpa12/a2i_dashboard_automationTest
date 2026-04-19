62a42bcc345dda96116d25f3868a0fd612f1360d
arpa12 <nihanarpa64@gmail.com>
2026-04-19 12:33:21 +0600

    implemented other modules test script

--- cypress/pages/report.js
+++ cypress/pages/report.js
@@ -1,3 +1,15 @@
-export const reportPage = () => { 
-  cy.get('/html/body/header/div/nav/ul/li[5]/a').click()
-}
+export const reportPage = () => {
+  cy.xpath("/html/body/header/div/nav/ul/li[5]/a").click();
+  cy.wait(2000);
+  cy.xpath("/html/body/main/div[2]/div[1]/a").click();
+  cy.wait(2000);
+  cy.get('[id="report_title"]').type("test");
+  cy.wait(2000);
+  cy.get('[id="tags"]').type("test");
+  cy.wait(2000);
+  cy.get('[id="report_para1"]').type("SELECT * from users");
+  cy.wait(2000);
+  cy.get('[id="verifyBtn"]').click();
+  cy.wait(2000);
+  cy.xpath("/html/body/main/div/form/div[3]/div[1]/button[1]").click();
+};
