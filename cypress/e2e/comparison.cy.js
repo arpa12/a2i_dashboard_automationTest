2e81a9e28b43f95e8462a2ea955070d8f90d131a
arpa12 <nihanarpa64@gmail.com>
2026-04-19 16:22:44 +0600

    implemented report module

--- cypress/e2e/comparison.cy.js
+++ cypress/e2e/comparison.cy.js
@@ -1,8 +1,8 @@
 import { loginPage } from "../pages/login";
 import { comparisonPage } from "../pages/comparison";
 
-describe("template spec", () => {
-  it.skip("check dashboard functionality", () => {
+describe("checking comparison module functionality", () => {
+  it("check comparison module functionality", () => {
     cy.visit("https://uat-dashboard.a2i.gov.bd/");
     loginPage();
     comparisonPage();
--- cypress/e2e/monitoring.cy.js
+++ cypress/e2e/monitoring.cy.js
@@ -2,14 +2,14 @@ import { loginPage } from "../pages/login";
 import { monitoringPage } from "../pages/resultMonitoring";
 import { resultFrameworkPage } from "../pages/resultFramework";
 
-describe("template spec", () => {
-  it.skip("check dashboard functionality", () => {
+describe("checking monitoring and result framework module functionality", () => {
+  it.skip("check monitoring module functionality", () => {
     cy.visit("https://uat-dashboard.a2i.gov.bd/");
     loginPage();
     monitoringPage();
   });
 
-  it("check dashboard functionality", () => {
+  it("check result framework module functionality", () => {
     cy.visit("https://uat-dashboard.a2i.gov.bd/");
     loginPage();
     resultFrameworkPage();
